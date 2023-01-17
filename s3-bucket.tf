215abf34ca1b26d4d44549e8a85946e1a0619c22
Jason Mueller <jmueller@healthfirst.org>
2023-01-17 20:57:23 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/jmueller-org/s3-bucket/aws"
   version = "2.8.0"
   bucket_prefix = "jmueller"
-  bucket = "my-s3-bucket"
+  #bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
